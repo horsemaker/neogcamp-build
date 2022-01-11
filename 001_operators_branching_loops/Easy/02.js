18487366f1d69179cd2333734e709379d4c2a8ab
horsemaker <yash_ghodekar@yahoo.in>
2022-01-12 01:24:31 +0530

    02

---
+++ 001_operators_branching_loops/Easy/02.js
@@ -0,0 +1,3 @@
+// Write a program to take a number input from user and determine whether the number is odd or even.
+const oddOrEven = n => n % 2 === 0 ? `${n} is even` : `${n} is odd`;
+console.log(oddOrEven(10011111111111));
