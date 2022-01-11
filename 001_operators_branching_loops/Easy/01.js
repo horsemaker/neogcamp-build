d7d476a2555e0674b3f65142bb19d09e15980760
horsemaker <yash_ghodekar@yahoo.in>
2022-01-12 01:18:30 +0530

    01

---
+++ 001_operators_branching_loops/Easy/01.js
@@ -0,0 +1,9 @@
+// Write a program to add 5 numbers.The value of numbers are num1 = 5, num2 = 13, num3 = 7, num4 = 21 and num5 = 48.
+const add = (...rest) => rest.reduce((a, b) => a + b);
+console.log(add(1, 2, 3, 99));
+
+const numArray = [5, 13, 7, 21, 48];
+const [num1, num2, num3, num4, num5] = numArray;
+
+console.log(add(...numArray));
+console.log(add(num1, num2, num3, num4, num5));
